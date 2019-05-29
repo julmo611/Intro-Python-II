6d5061dc9423553fe370921db0a013be76a419d4
Julian Moreno <julmo611@gmail.com>
2019-05-29 14:45:02 -0700

    My first class on python

--- src/player.py
+++ src/player.py
@@ -1,2 +1,15 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+
+class Player:
+    def __init__(self, name, room, item):
+        self.name = name
+        self.room = room
+        self.item = item
+
+    def __str__(self):
+        return str(self.__dict__)
+
+print(Player("julian", "someroom", "money"))
+
